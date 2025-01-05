43eea00b415eb9c995fce7f133cd2557acb6d65a
ajaydev <ajaydevkomath@gmail.com>
2025-01-05 22:15:47 +0530

    themes added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Themes from './Shared/Themes'
+
+function Header() {
+    return (
+        <div className="navbar  bg-base-100  sticky top-0">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow gap-3 text-black">
+                        <li>Demos</li>
+                        <li>
+                            About
+                        </li>
+                        <li>Blog</li>
+                        <li>Pages</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl font-bold">Chat App</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-4 font-bold">
+                    <li>Demos</li>
+                    <li>
+                        About
+                    </li>
+                    <li>Blog</li>
+                    <li>Pages</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="navbar-end space-x-3">
+                <Themes />
+                <a className="btn font-bold">Login</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
